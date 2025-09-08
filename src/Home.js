f94b85fba8bbd1dc377afcb0e47bc49c35ee1c2d
Montfort <mugishamontfort28@gmail.com>
2025-09-09 00:08:44 +0200

    learnt about usestate hook and also mapping

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,23 @@
+import {ToastContainer} from 'react-toastify';
+import { useState } from 'react';
 const Home = () => {
-    const title = 'Home Page';
+    const[blogs, setBlogs] =useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mont', id: 3}
+    ])
+
     return ( 
         <div className= 'home'>
-            <h2>{ title }</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+            <ToastContainer />
         </div>
      );
 }
- 
+
 export default Home;
