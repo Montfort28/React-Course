cc5acf15d476c5beee9a9db7d5315fb53be13bb9
Montfort <mugishamontfort28@gmail.com>
2025-09-16 23:42:33 +0200

    adding blogs and navigating to the home page after adding the blog successfuly

--- src/Create.js
+++ src/Create.js
@@ -1,48 +1,59 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { useNavigate} from 'react-router-dom'
 
-const Create = () =>{
-    const[title, setTitle] = useState('')
-    const[body, setBody] = useState('')
-    const[author, setAuthor] = useState('mario')
-    
-    const handleSubmit= (e) =>{
-      e.preventDefault()
+const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate()
 
-      const blog= {title, body, author}
-      console.log(blog)
-    }
-    
-    return(
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    const blog = { title, body, author };
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("blog added successfully");
+      setIsLoading(false);
+      navigate('/')
+    });
+  };
+
+  return (
     <div className="NewBlog">
-        <h1>Add a New Blog</h1>
-        <form onSubmit={handleSubmit}>
-            <label>Blog title</label>
-            <input 
-            type="text" 
-            required
-            value={title}
-            onChange={(e) =>setTitle(e.target.value)}
-            />
-            <label>Blog body</label>
-            <textarea 
-            required
-            value={body}
-            onChange={(e) =>setBody(e.target.value)}
-            >
-            </textarea>
-            <label>Blog author</label>
-            <select
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            >
-                <option value="mario">mario</option>
-                <option value="yoshi">yoshi</option>
-                <option value="mont">mont</option>
-            </select>
-            <button>ADD BLOG</button>
-        </form>
+      <h1>Add a New Blog</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+          <option value="mont">mont</option>
+        </select>
+        {!isLoading && <button>ADD BLOG</button>}
+        {isLoading && <button disabled>ADDING BLOG.....</button>}
+      </form>
     </div>
-    )
-}
+  );
+};
 
 export default Create;
